881f08f080b3397bde99c940bcd5faa782f590ed
Oleh Kuzmenko <oleg24453@gmail.com>
2020-06-23 18:58:31 +0300

    Add functionality to read the file

---
+++ src/main/java/spring/boot/app/demo/util/CustomFileReader.java
@@ -0,0 +1,24 @@
+package spring.boot.app.demo.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import org.apache.log4j.Logger;
+
+public class CustomFileReader {
+    private static final Logger LOGGER = Logger.getLogger(CustomFileReader.class);
+
+    public List<String> getAll(String directory) {
+        List<String> allLines = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(directory))) {
+            allLines = Files.readAllLines(Paths.get(directory));
+            LOGGER.info("Information from file with directory " + directory + "was read");
+        } catch (IOException e) {
+            LOGGER.error("Can not read from file with directory " + directory);
+        }
+        return allLines;
+    }
+}
