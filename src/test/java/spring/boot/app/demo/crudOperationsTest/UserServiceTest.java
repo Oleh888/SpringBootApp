29f0ee10d417de13f73e007cc9b4a442ce1c2975
Oleh Kuzmenko <oleg24453@gmail.com>
2020-07-03 18:33:22 +0300

    corrected UserServiceTest

--- src/test/java/spring/boot/app/demo/crudOperationsTest/UserServiceTest.java
+++ src/test/java/spring/boot/app/demo/crudOperationsTest/UserServiceTest.java
@@ -1,36 +1,39 @@
 package spring.boot.app.demo.crudOperationsTest;
 
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 import spring.boot.app.demo.model.User;
 import spring.boot.app.demo.service.UserService;
 import spring.boot.app.demo.util.CustomCsvParser;
 import spring.boot.app.demo.util.CustomFileReader;
-
 import java.util.List;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
 public class UserServiceTest {
     private static final String FILE_TEST = "src/test/resources/test1.csv";
-    private AnnotationConfigApplicationContext context;
-    private CustomFileReader reader;
-    private CustomCsvParser parser;
-    private UserService userService;
+    private static UserService userService;
+    private static List<User> users;
 
-    @Before
-    public void setHelperObjects() {
-        context = new AnnotationConfigApplicationContext("spring.boot.app.demo");
-        reader = new CustomFileReader();
-        parser = new CustomCsvParser();
+    @BeforeClass
+    public static void setHelperObjects() {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext("spring.boot.app.demo");
+        CustomFileReader reader = new CustomFileReader();
+        CustomCsvParser parser = new CustomCsvParser();
         userService = context.getBean(UserService.class);
+        List<String> actual = reader.getAll(FILE_TEST);
+        users = parser.getAllUsers(actual);
+        users.forEach(userService::save);
     }
 
     @Test
     public void creatingAndGettingUserIsOk() {
-        List<String> actual = reader.getAll(FILE_TEST);
-        List<User> users = parser.getAllUsers(actual);
-        users.forEach(userService::save);
         List<User> usersFromDb = userService.findAll();
         Assert.assertEquals(users.size(), usersFromDb.size());
         for (int i = 0; i < users.size(); i++) {
@@ -40,9 +43,6 @@ public class UserServiceTest {
 
     @Test
     public void deletingUserIsOk() {
-        List<String> actual = reader.getAll(FILE_TEST);
-        List<User> users = parser.getAllUsers(actual);
-        users.forEach(userService::save);
         userService.delete(users.get(0));
         users.remove(users.get(0));
         List<User> usersFromDb = userService.findAll();
@@ -54,9 +54,6 @@ public class UserServiceTest {
 
     @Test
     public void getUserByIdIsOk() {
-        List<String> actual = reader.getAll(FILE_TEST);
-        List<User> users = parser.getAllUsers(actual);
-        users.forEach(userService::save);
         for (User expectedUser : users) {
             Assert.assertEquals(expectedUser, userService.findById(expectedUser.getId())
                     .orElseThrow(RuntimeException::new));
