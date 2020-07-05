a53b9844030b993d54a62c4cc2eb5b021f3daf58
Oleh Kuzmenko <oleg24453@gmail.com>
2020-07-05 19:51:54 +0300

    deleted useless annotations

--- src/main/java/spring/boot/app/demo/repository/UserRepository.java
+++ src/main/java/spring/boot/app/demo/repository/UserRepository.java
@@ -1,12 +1,8 @@
 package spring.boot.app.demo.repository;
 
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import spring.boot.app.demo.model.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
 }
