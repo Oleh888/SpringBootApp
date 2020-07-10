f31d8614d06746e6104bd262af50ecaea0738cce
Oleh Kuzmenko <oleg24453@gmail.com>
2020-07-10 12:49:49 +0300

    added corrections

--- src/main/java/spring/boot/app/demo/DemoApplication.java
+++ src/main/java/spring/boot/app/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package spring.boot.app.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -20,7 +21,8 @@ public class DemoApplication {
     public Docket springBootApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("spring.boot.app.demo.controller"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
 }
